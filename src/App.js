35531915ebc1204c39e37565a95cf030658c007f
Talita Oliveira <litaa.oliveira@gmail.com>
2020-09-06 19:39:53 -0300

    - Get the Loader component by the file name - Add state para username and error - On the function using the username from the variable - onChange on the input to get the value from the user - conditionally showing error

--- src/App.js
+++ src/App.js
@@ -1,31 +1,36 @@
 import React, { useState } from 'react';
-import Loader from './components/Loader'
+import Loader from './components/Loader/Loader'
 
 import './App.css';
 
 const App = () => {
   const [showLoader, setShowLoader] = useState(false)
+  const [username, setUsername] = useState('')
+  const [hasError, setHasError] = useState(false)
 
   const handleForm = (e) => {
     e.preventDefault();
     setShowLoader(true)
-    fetch("https://api.github.com/users/talitaoliveira")
+    fetch(`https://api.github.com/users/${username}`)
         .then(res => res.json())
-        .then(res => {
-          console.log(res)
+        .then(data => {
           setShowLoader(false)
         })
-        .catch((err) => console.log(err))
+        .catch((err) => {
+          setHasError('Ooops... Deu alguma merda.')
+          setShowLoader(false)
+        })
   }
 
   return (
     <div className="app">
       <form onSubmit={handleForm} className="form-github-user">
         <label htmlFor="github-user" className="label-github-user">Digite seu usuário do Github:</label>
-        <input type="text" id="github-user" className="input-github-user"/>
+        <input type="text" id="github-user" className="input-github-user" onChange={e=>setUsername(e.target.value)}/>
         <button type="submit" className="btn-submit">Enviar</button>
       </form>
       {showLoader && <Loader/>}
+      {hasError && <p data-testid="error-message">{hasError}</p>}
     </div>
   );
 }
