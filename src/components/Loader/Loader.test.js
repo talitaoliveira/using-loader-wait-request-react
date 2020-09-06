153c4c94110e5dddece88ce1b5c3671584982a3e
Talita Oliveira <litaa.oliveira@gmail.com>
2020-09-06 19:38:18 -0300

    Rename component files

--- src/components/Loader/Loader.test.js
+++ src/components/Loader/Loader.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import Loader from './index';
+import Loader from './Loader';
 
 describe('Page', () => {
   it('should exist a button with name "call API"', () => {
