825a479ef3d00995e1819e3e4c11ee81559bea48
Talita Oliveira <litaa.oliveira@gmail.com>
2020-09-06 22:19:19 -0300

    Add some style

--- src/components/UserData/UserData.js
+++ src/components/UserData/UserData.js
@@ -3,24 +3,24 @@ import './UserData.css';
 
 const UserData = ({data}) => {
     return (
-        <div className="user-data">
-        <img src={data.avatar_url} alt={data.name}/>
-        <table>
+        <div className="user-content">
+        <img src={data.avatar_url} alt={data.name} className="user-image"/>
+        <table className="user-data">
           <tbody>
-            <tr>
-              <td><strong>Name:</strong></td>
+            <tr className="rows">
+              <td className="user-data-title"><strong>Name:</strong></td>
               <td>{data.name}</td>
             </tr>
-            <tr>
-              <td><strong>Localização:</strong></td>
+            <tr className="rows">
+              <td className="user-data-title"><strong>Localização:</strong></td>
               <td>{data.location}</td>
             </tr>
-            <tr>
-              <td><strong>Bio:</strong></td>
+            <tr className="rows">
+              <td className="user-data-title"><strong>Bio:</strong></td>
               <td>{data.bio}</td>
             </tr>
-            <tr>
-              <td><strong>Perfil criado em:</strong></td>
+            <tr className="rows">
+              <td className="user-data-title"><strong>Perfil criado em:</strong></td>
               <td>{data.created_at}</td>
             </tr>
           </tbody>
