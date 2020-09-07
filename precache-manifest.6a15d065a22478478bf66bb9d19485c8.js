7e208a927a4d19db6470d83985ed8eee91d3ec27
talitaoliveira <litaa.oliveira@gmail.com>
2020-09-07 00:03:24 +0000

    Deploying to gh-pages from  @ 001e861e0a9ddf2bbca605873348a2236591b87f 🚀

---
+++ precache-manifest.6a15d065a22478478bf66bb9d19485c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de452455ab8fb6912c09b4c759677af5",
+    "url": "/using-loader-wait-request-react/index.html"
+  },
+  {
+    "revision": "7ec4910840467b87c773",
+    "url": "/using-loader-wait-request-react/static/css/main.91ab2300.chunk.css"
+  },
+  {
+    "revision": "88f181ea44b6b45a297e",
+    "url": "/using-loader-wait-request-react/static/js/2.063b9ebd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/using-loader-wait-request-react/static/js/2.063b9ebd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ec4910840467b87c773",
+    "url": "/using-loader-wait-request-react/static/js/main.1f7cf521.chunk.js"
+  },
+  {
+    "revision": "7431ceb14c274ead42e9",
+    "url": "/using-loader-wait-request-react/static/js/runtime-main.f85fbe6f.js"
+  }
+]);
