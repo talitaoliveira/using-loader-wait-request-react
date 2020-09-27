799833895160e6956119c06fcbb52e23c32c5a66
Talita Oliveira <litaa.oliveira@gmail.com>
2020-09-27 19:40:36 -0300

    Create componet for the form

--- src/App.js
+++ src/App.js
@@ -1,45 +1,12 @@
-import React, { useState } from 'react';
-import Loader from './components/Loader/Loader'
-import UserData from './components/UserData/UserData'
+import React from 'react'
+import FormUserData from './components/FormUserData/FormUserData'
 
 import './App.css';
 
 const App = () => {
-  const [showLoader, setShowLoader] = useState(false)
-  const [username, setUsername] = useState('')
-  const [hasError, setHasError] = useState(false)
-  const [userData, setUserData] = useState({})
-
-
-  const handleForm = (e) => {
-    e.preventDefault();
-    setShowLoader(true)
-    fetch(`https://api.github.com/users/${username}`)
-        .then(res => res.json())
-        .then(data => {
-          setUserData(data)
-          setShowLoader(false)
-        })
-        .catch((err) => {
-          setHasError('Ooops... Deu alguma merda.')
-          setShowLoader(false)
-        })
-  }
-
-  const isObjectEmpty = (myObject) => {
-    return Object.entries(myObject).length > 0
-  }
-
   return (
     <div className="app">
-      <form onSubmit={handleForm} className="form-github-user">
-        <label htmlFor="github-user" className="label-github-user">Digite seu usuário do Github:</label>
-        <input type="text" id="github-user" className="input-github-user" onChange={e=>setUsername(e.target.value)}/>
-        <button type="submit" className="btn-submit">Enviar</button>
-      </form>
-      {showLoader && <Loader/>}
-      {hasError && <p data-testid="error-message">{hasError}</p>}
-      {isObjectEmpty(userData) && <UserData data={userData} />}
+      <FormUserData></FormUserData>
     </div>
   );
 }
